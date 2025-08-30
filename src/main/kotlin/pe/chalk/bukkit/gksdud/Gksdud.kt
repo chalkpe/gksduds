70a25d327a51800503370af2244aef6dc93eb556
Chalk <chalk@chalk.pe>
2025-08-31 00:18:26 +0900

    feat: calculate threshold only by korean

--- src/main/kotlin/pe/chalk/bukkit/gksdud/Gksdud.kt
+++ src/main/kotlin/pe/chalk/bukkit/gksdud/Gksdud.kt
@@ -16,6 +16,7 @@ val COMPLEX_JAMOS = mapOf(
     "ㅗㅏ" to "ㅘ", "ㅗㅐ" to "ㅙ", "ㅗㅣ" to "ㅚ", "ㅜㅓ" to "ㅝ", "ㅜㅔ" to "ㅞ", "ㅜㅣ" to "ㅟ", "ㅡㅣ" to "ㅢ",
     "ㄱㅅ" to "ㄳ", "ㄴㅈ" to "ㄵ", "ㄴㅎ" to "ㄶ", "ㄹㄱ" to "ㄺ", "ㄹㅁ" to "ㄻ", "ㄹㅂ" to "ㄼ", "ㄹㅅ" to "ㄽ", "ㄹㅌ" to "ㄾ", "ㄹㅍ" to "ㄿ", "ㄹㅎ" to "ㅀ", "ㅂㅅ" to "ㅄ",
 )
+val CHECKS = mapOf('가'..'힣' to Int.MAX_VALUE, 'ㄱ'..'ㅎ' to 2, 'ㅏ'..'ㅣ' to 2)
 
 /**
  * @author ChalkPE <chalk@chalk.pe>
@@ -26,12 +27,9 @@ class Gksdud : JavaPlugin(), Listener {
         fun isAlphabet(str: Char) = ALPHABETS.contains(str)
         fun toHangeul(str: Char) = HANGEUL_JAMOS[ALPHABETS.indexOf(str)]
         fun replaceAlphabets(str: String) = str.map { if (isAlphabet(it)) toHangeul(it) else it }.joinToString("")
-        fun stack(i: String, m: String, f: String = " ") =
-            (44032 + FINAL_JAMOS.indexOf(f) + MEDIAL_JAMOS.indexOf(m) * 28 + INITIAL_JAMOS.indexOf(i) * 588).toChar().toString()
-        fun gksdud(str: String) =
-            HANGEUL_SYLLABLE.replace(replaceAlphabets(str), { it.destructured.let { (i, m, f) -> stack(i, COMPLEX_JAMOS[m] ?: m, COMPLEX_JAMOS[f] ?: f) } })
-        fun check(str: String, range: CharRange, count: Int? = null) =
-            str.count { it in range } / str.length.toDouble() >= 0.75 && (count == null || str.filter { it in range }.toCharArray().distinct().size <= count)
+        fun stack(i: String, m: String, f: String = " ") = (44032 + FINAL_JAMOS.indexOf(f) + MEDIAL_JAMOS.indexOf(m) * 28 + INITIAL_JAMOS.indexOf(i) * 588).toChar().toString()
+        fun gksdud(str: String) = HANGEUL_SYLLABLE.replace(replaceAlphabets(str), { it.destructured.let { (i, m, f) -> stack(i, COMPLEX_JAMOS[m] ?: m, COMPLEX_JAMOS[f] ?: f) } })
+        fun check(str: String) = CHECKS.any { (range, count) -> str.filter { it in range }.let { it.length / str.length.toDouble() >= 0.75 && it.toList().distinct().size <= count } }
     }
 
     override fun onEnable() = server.pluginManager.registerEvents(this, this)
@@ -41,7 +39,7 @@ class Gksdud : JavaPlugin(), Listener {
         if (event.isCancelled) return
         event.message = when {
             event.message.contains(",") -> event.message.split(",").mapIndexed { i, part -> if (i % 2 == 1) gksdud(part) else part }.joinToString("")
-            else -> event.message.split(" ").map { gksdud(it).takeIf { check(it, '가'..'힣') || check(it, 'ㄱ'..'ㅎ', 2) || check(it, 'ㅏ'..'ㅣ', 2) } ?: it }.joinToString(" ")
+            else -> event.message.split(" ").map { gksdud(it).takeIf { check(it.filter { c -> CHECKS.any { c in it.key } }) } ?: it }.joinToString(" ")
         }
     }
 }
